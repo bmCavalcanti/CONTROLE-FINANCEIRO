e4adb1d4c257dde5c7aa6dac5e4e4bc2406970f9
Bianca Cavalcanti <bm_cavalcanti@hotmail.com>
2025-10-31 22:11:19 -0300

    .env

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,16 +1,17 @@
 import { DataSource } from "typeorm";
 import requireDir = require("require-dir");
+import 'dotenv/config';
 
 const entitiesObject = requireDir('../app/entities');
 const entities = Object.values(entitiesObject);
 
 export const Connection = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "bianca-cavalcanti",
-    password: "Sukun4",
-    database: "controle_financeiro",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: entities.map((element: any) => Object.values(element)[0] as Function).flat(),
     synchronize: false,
     logging: false,
